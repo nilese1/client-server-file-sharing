93af4fa29380713747d8050b4b5479eaab064c87
ExcuseMeee <linoscar01@gmail.com>
2024-11-02 22:26:22 -0400

    create and delete dirs. list_dir now returns a dictionary

--- server/handleFolders.py
+++ server/handleFolders.py
@@ -1,18 +1,55 @@
 # for dir and subfolder functions
 from pathlib import Path
+from pprint import pprint
 
+ROOT_PATH = "server_root"
 
-def list_dir(rootPath: str, recursive: bool = False):
+def list_dir(rootPath: Path):
     '''
-    List all files and directories in given path. To list every single thing, set recursive to True. 
-    TODO: Probably will return a dict object. Idea is to serialize it into JSON when sending over network.
+    Recursively gets all files and directories in given path. 
+    Returns dictionary that mirrors the hierarchical structure of the files.
     '''
 
-    path = Path(rootPath)
-
-    generator = path.rglob("*") if recursive else path.glob("*")
-    for subPath in generator:
-        print(f'path: {subPath.relative_to(path).as_posix()}{"/" if subPath.is_dir() else ""}')
+    files_dict = {}
     
+    for subPath in rootPath.rglob("*"):
+        parts = subPath.relative_to(rootPath).parts
+        curr = files_dict
+        for part in parts[:-1]:
+            curr = curr.setdefault(part, {})
+        
+        if subPath.is_file():
+            curr[parts[-1]] = None
+        else:
+            curr[parts[-1]] = {}
 
-list_dir("server_root", False)
+    
+    return files_dict
+        
+
+
+def create_dir(path: Path):
+    '''
+    Creates a directory at the specified path. Nested directories can be created.
+    '''
+
+    path.mkdir(parents=True) # parents=True allows for making subdirectories
+
+def delete_dir(path: Path):
+    '''
+    Recursively delete specified directory. Will delete everything in that directory!
+    '''
+
+    for subPath in path.iterdir():
+        if subPath.is_file(): 
+            subPath.unlink() # normal files must be deleted because rmdir() only works on empty directories
+        else: 
+            delete_dir(subPath) # recursion ftw :D
+
+    path.rmdir()
+
+# pprint(list_dir(Path(ROOT_PATH)))
+
+# delete_dir(Path(f'{ROOT_PATH}/newFolder'))
+
+# create_dir(Path(f'{ROOT_PATH}/newFolder/subfolder'))
